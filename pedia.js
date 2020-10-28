474bf3f67dc7c635b70a4bce5ca98479418bcb88
Dominique Hazael-Massieux <dom@w3.org>
2020-10-28 14:25:38 +0100

    Use new URL for data

--- pedia.js
+++ pedia.js
@@ -1,4 +1,4 @@
-fetch("https://tidoust.github.io/reffy-reports/ed/crawl.json", {mode:"cors"})
+fetch("https://w3c.github.io/webref/ed/crawl.json", {mode:"cors"})
     .then(r => r.json())
     .then(({results}) => {
         var used_by = {};
